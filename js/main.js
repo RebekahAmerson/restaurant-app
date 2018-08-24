b98317810c252cbbd0d500a630a54ee31ac39b9c
Rebekah Amerson <35541632+RebekahAmerson@users.noreply.github.com>
2018-08-24 12:04:29 -0500

    Update main.js
    Hide Mapbox key

--- js/main.js
+++ js/main.js
@@ -80,7 +80,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiYmVja2E4MTEiLCJhIjoiY2prY28ydWVtMDRtbzNxcnI3aGQzbWEyaCJ9.oFm0HaRTBybwDJyeRttpfQ',
+    mapboxToken: 'MapBox Key goes here',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
